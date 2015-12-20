d65047945b5bc4ea7846e6fa524f9db59dab22ec
RocketCat <shine1986@gmail.com>
2015-12-20 16:55:32 +0800

    onStart and onStop

--- app/src/main/java/org/zerodiary/stopwatch/StopWatchActivity.java
+++ app/src/main/java/org/zerodiary/stopwatch/StopWatchActivity.java
@@ -11,6 +11,7 @@ public class StopWatchActivity extends Activity {
 
     private int seconds = 0;
     private boolean running;
+    private boolean wasRunning;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +19,7 @@ public class StopWatchActivity extends Activity {
         if(savedInstanceState != null){
             seconds = savedInstanceState.getInt("seconds");
             running = savedInstanceState.getBoolean("running");
+            wasRunning = savedInstanceState.getBoolean("wasRunning");
         }
         setContentView(R.layout.activity_stop_watch);
         runTime();
@@ -55,10 +57,29 @@ public class StopWatchActivity extends Activity {
         });
     }
 
+
     public void onSaveInstanceState(Bundle savedInstanceState){
         savedInstanceState.putInt("seconds",seconds);
-        savedInstanceState.putBoolean("running",running);
+        savedInstanceState.putBoolean("running", running);
+        savedInstanceState.putBoolean("wasRunning",wasRunning);
     }
+
+    @Override
+    protected void onStop(){
+        super.onStop();
+        wasRunning = running;
+        running = false;
+    }
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+        if(wasRunning){
+            running = true;
+        }
+    }
+
+
 }
 
 
